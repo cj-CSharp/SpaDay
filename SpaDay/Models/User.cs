d03b6cd1eb307eefc07f057d82b6e74f0b0808a0
cjconnor18 <cjallen1988@gmail.com>
2020-10-29 15:41:59 -0500

    created user model

--- SpaDay/Models/User.cs
+++ SpaDay/Models/User.cs
@@ -7,5 +7,17 @@ namespace SpaDay.Models
 {
     public class User
     {
+        public string Username { get; set; }
+        public string Password { get;  }
+        public string Email { get; set; }
+
+        public User () { }
+
+        public User (string username, string password, string email)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+        }
     }
 }
