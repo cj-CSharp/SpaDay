eebf7271131430e152757d421c0c510f4152086f
cjconnor18 <cjallen1988@gmail.com>
2020-11-09 16:12:59 -0600

    Spa Studio complete chapter 15 studio 12

--- SpaDay/Models/User.cs
+++ SpaDay/Models/User.cs
@@ -6,7 +6,7 @@ namespace SpaDay.Models
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
+      
         public User()
         {
         }
